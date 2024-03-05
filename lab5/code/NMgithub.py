799d592711d56385767975c2014ede6cc5849d0a
AshChanCUB <143350160+AshChanCUB@users.noreply.github.com>
2024-03-04 21:29:26 -0700

    Initial commit

--- lab5/code/NMgithub.py
+++ lab5/code/NMgithub.py
@@ -8,5 +8,20 @@ git = Git(rep_path)
 
 txt_file = 'router_data.txt'
 jpg_file = 'CPU_Utilization_R1.jpg'
-repo.git.add(txt_file)
-repo.git.add(jpg_file)
+
+
+diff_txt = repo.git.diff("--", txt_file)
+diff_jpg = repo.git.diff("--", jpg_file)
+
+if diff_txt or diff_jpg:
+    repo.git.add(txt_file)
+    repo.git.add(jpg_file)
+
+    print('Changes detected')
+    repo.index.commit("Detected changes and pushed through NMgithub.py")
+
+    origin = repo.remote.origin
+    origin.push()
+else:
+    print('No changes detected')
+
