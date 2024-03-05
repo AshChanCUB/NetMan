330ddda1a03c2d0eca36efd391b48fb37a527ca5
AshChanCUB <143350160+AshChanCUB@users.noreply.github.com>
2024-03-05 10:46:02 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import subprocess
+
+
+if __name__ == "__main__":
+
+    subprocess.run(['python3', 'NMtcpdump.py'])
+    subprocess.run(['python3', 'NMdhcpserver.py'])
+    subprocess.run(['python3', 'NMsnmp.py'])
+    subprocess.run(['python3', 'NMgithub.py'])
