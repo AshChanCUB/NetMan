7a37372f2e6eea0dc6f53514641dbb7c6d0971f0
AshChanCUB <143350160+AshChanCUB@users.noreply.github.com>
2024-03-05 10:40:16 -0700

    Detected changes and pushed through NMgithub.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import subprocess
 
 if __name__ == "__main__":
 
-
     subprocess.run(['python3', '/home/student/Desktop/netman/git/NetMan/netman_midterm_lab5/NMtcpdump.py'])
     subprocess.run(['python3', '/home/student/Desktop/netman/git/NetMan/netman_midterm_lab5/NMdhcpserver.py'])
     subprocess.run(['python3', '/home/student/Desktop/netman/git/NetMan/netman_midterm_lab5/NMsnmp.py'])
